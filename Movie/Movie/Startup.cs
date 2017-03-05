2d7077b3dbc4e61fbd76a20c5e384e8b6ad804d6
rohitkat10 <rohitkat10@gmail.com>
2017-03-05 21:47:01 +0530

    First Asp.net MVC project

---
+++ Movie/Movie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Movie.Startup))]
+namespace Movie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
